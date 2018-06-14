2be297319abb212c8b9d8d0b9a01d75e5e8a0335
Thibaut Neiger <thibaut.neiger@mechdyne.com>
2018-06-14 20:08:52 +0100

    Arf

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
+var path = require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//app.get('/', function(req, res){
+//	res.sendFile(__dirname + '/index.html');
+//});
+
+app.use(express.static(__dirname));
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	console.log("Client connected: " + socket.id);
+	socket.on('chat message', function(msg){
+		//console.log('Blah: ' + msg);
+		io.emit('chat message', msg);
+	});
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
